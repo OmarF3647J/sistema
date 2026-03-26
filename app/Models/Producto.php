0bd22e9026414a99f2b8941ea363372c6b76f830
OmarF3647J <omarfiscalpolito@hotmail.com>
2026-03-26 12:31:04 -0600

    m

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -4,9 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Producto extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductoFactory> */
     use HasFactory;
+
+    protected $primaryKey = 'idproduct';
+
+    protected $fillable = ['nomproduct'];
+
+    public function centrosturist() : HasMany {
+        return $this->hasMany(Centrosturist::class, 'idproduct', 'idproduct');
+    }
 }
